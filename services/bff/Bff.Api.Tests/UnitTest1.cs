b575f450ecc4000c626b16713959f08b91fead59
Natanael Feješ <fejesnatanael1@gmail.com>
2026-03-04 13:08:38 +0100

    feat: scaffold .NET solution and microservice projects

---
+++ services/bff/Bff.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Bff.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ services/rdm/Rdm.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Rdm.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
