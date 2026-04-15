0d9f69592a879c1c0de14f95cc1bb45f5045f8b0
Natanael Feješ <fejesnatanael1@gmail.com>
2026-04-15 14:10:43 +0200

    fix: Add missing project reference and resolve StyleCop issues

--- services/rdm/Rdm.Api.Tests/UnitTest1.cs
+++ services/rdm/Rdm.Api.Tests/UnitTest1.cs
@@ -1,10 +1,19 @@
-﻿namespace Rdm.Api.Tests;
+﻿// <copyright file="UnitTest1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Rdm.Api.Tests;
+
+using System;
+using Rdm.Api.Domain.Models;
+using Rdm.Api.Infrastructure.Persistence.PersistenceModels;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        // Simple sanity check
+        Assert.True(true);
     }
 }
