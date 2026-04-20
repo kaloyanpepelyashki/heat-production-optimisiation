fe59c46838728c53f7e00fe32c772bff6819495a
Samuel Toman <satom25@student.sdu.dk>
2026-04-20 15:47:46 +0200

    ProductionUnitsView/Edited + maintenance schedule implemented

---
+++ client/Dv.App/Models/MaintenanceStore.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.ObjectModel;
+
+namespace Dv.App.Models;
+
+// Storing data to view in ProductionUnitsView UI
+public class MaintenanceEvent
+{
+    public string AssetName { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Period { get; set; } = string.Empty;
+    public string Scenario { get; set; } = string.Empty;
+}
+
+public static class MaintenanceStore
+{
+    public static ObservableCollection<MaintenanceEvent> MaintenanceSchedules { get; } = new();
+}
