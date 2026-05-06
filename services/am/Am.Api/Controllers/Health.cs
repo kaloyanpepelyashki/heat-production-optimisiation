956b73a157aef52e4ec5af00852d89d59a404075
Kaloyan Pepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2026-05-06 13:29:01 +0200

    Set up a health controller, to wake-up service when in staging

---
+++ services/am/Am.Api/Controllers/Health.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Am.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class Health : Controller
+{
+    [HttpGet("wakeup")]
+    public IActionResult WakeUp()
+    {
+        return Ok();
+    }
+}
