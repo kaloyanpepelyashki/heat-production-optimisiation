2900570982a36d242443d0ebdfc26c9641dd3c64
Kaloyan Pepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2026-05-06 13:57:48 +0200

    Implemented a health check on SDM

---
+++ services/sdm/Sdm.Api/Controllers/Health.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sdm.Api.Controllers;
+
+
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+[ApiController]
+public class Health : Controller
+{
+    [HttpGet("wakeup")]
+    public IActionResult WakeUp()
+    {
+        return Ok();
+    }
+}
