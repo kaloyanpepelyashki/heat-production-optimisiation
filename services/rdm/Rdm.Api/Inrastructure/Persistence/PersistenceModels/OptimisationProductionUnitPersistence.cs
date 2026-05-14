28965822ade246866d7e6dcec0bc76b6342a79e4
Kaloyan Pepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2026-05-14 17:13:24 +0200

    Refactor of the the model
    Implemented the refactor change, altering the the different models - dto, persistence and base domain model, to account for the change in the database schema, including the new fields part of the production units, included in each optimisation run

--- services/rdm/Rdm.Api/Inrastructure/Persistence/PersistenceModels/OptimisationProductionUnitPersistence.cs
+++ services/rdm/Rdm.Api/Inrastructure/Persistence/PersistenceModels/OptimisationProductionUnitPersistence.cs
@@ -16,4 +16,13 @@ public class OptimisationProductionUnitPersistence: BaseModel
     public string ProductionUnitType { get; set; }
     [Column("capacity")]
     public double Capacity { get; set; }
+    
+    [Column("expenses")]
+    public double Expenses { get; set; }
+    [Column("co2Emissions")]
+    public double Co2Emissions { get; set; }
+    [Column("heat_production")]
+    public double HeatProduction { get; set; }
+    [Column("electricity_consumption")]
+    public double ElectricityConsumption { get; set; }
 }
